926044cd05abd19e37361d10a099c81956667d22
aayush1408 <aayushjaiswal0114@gmail.com>
2018-03-21 20:34:51 +0530

    modified app

--- app.js
+++ app.js
@@ -2,65 +2,61 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const ejs = require('ejs');
-
+const mongoose = require('mongoose');
 //Set storage engine
 const storage = multer.diskStorage({
-    destination:'./public/uploads',
-    filename:(req,file,cb)=>{
-        cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    destination: './public/uploads',
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
 });
 //Init upload
 const upload = multer({
-    storage:storage,
-    limits:{fileSize:1000000},
-    fileFilter:(req,file,cb)=>{
-        checkFileType(file,cb);
+    storage: storage,
+    limits: { fileSize: 1000000 },
+    fileFilter: (req, file, cb) => {
+        checkFileType(file, cb);
     }
 }).single('myImage');
 
 
 //Checkfile type
-function checkFileType(file,cb){
-const filetypes = /jpeg|png|jpg|gif/;
-const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-const mimetype = filetypes.test(file.mimetype);
-if(mimetype && extname){
-    return cb(null,true);
-}else{
-    cb('Error :Images only')
-}
+function checkFileType(file, cb) {
+    const filetypes = /jpeg|png|jpg|gif/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
+    if (mimetype && extname) {
+        return cb(null, true);
+    } else {
+        cb('Error :Images only')
+    }
 }
 
 const app = express();
 
 //EJS
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 //public
 app.use(express.static('./public'));
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('index');
 });
-app.post('/upload',(req,res)=>{
-    upload(req,res,(err)=>{
-        if(err){
-            res.render('index',{msg:err});
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        if (err) {
+            res.json({ success: false, msg: err });
         }
-        else{
-            if(req.file == undefined){
-                res.render('index',{
-                    msg:'Error:No file selected'
-                });
-            }else{
-                res.render('index',{
-                    msg:'File uploaded'
-                });
+        else {
+            if (req.file == undefined) {
+                res.json({ suceess: true, msg: 'No file selected' });
+            } else {
+                res.json({ success: true, msg: 'Image uploaded' });
             }
         }
     })
 })
-app.listen(3000,()=>{
-    console.log('server running on port');
+app.listen(3000, () => {
+    console.log('server running on port 3000');
 });
